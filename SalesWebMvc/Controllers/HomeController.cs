a0776229a0175741195644d6d2a48353a92cc560
Rodrigo Lima <contato.devlima@gmail.com>
2020-02-10 16:31:55 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+            ViewData["New"] = "Testando nova TAG";
 
             return View();
         }
